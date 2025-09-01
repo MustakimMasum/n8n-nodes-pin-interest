ab165b8766c8ed7959725bc392636cc6e1560c41
firepicaso <m.m.kiron@gmail.com>
2025-09-01 18:56:32 +0600

    Add index.ts file

---
+++ index.ts
@@ -0,0 +1,5 @@
+import { PinInterest } from './nodes/PinInterest/PinInterest.node';
+import { PinInterestOAuth2Api } from './credentials/PinInterestOAuth2Api.credentials';
+
+export const nodes = [PinInterest];
+export const credentials = [PinInterestOAuth2Api];
